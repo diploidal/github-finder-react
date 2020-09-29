d4fb630f16429b8232ba6b0c348273c85ed3272c
Diploidal <diploidal@gmail.com>
2020-09-29 23:08:25 +0200

    Add basic table functionality, map over received data

--- src/Components/TableComponent.js
+++ src/Components/TableComponent.js
@@ -15,25 +15,19 @@ export const Table = (props) => {
           <th scope="col">Handle</th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        <tbody>
+
+        {data ? data.map((element, index) => {
+          return (
+              <tr key={index}>
+                <th scope="row">{index}</th>
+                <td>{element.id}</td>
+                <td>{element.watchers}</td>
+                <td>{element.has_issues}</td>
+              </tr>
+          ) 
+        }) : <h1>Oops there are no repositories <span role="img">😞</span></h1>}
+
       </tbody>
     </table>
     </div>
