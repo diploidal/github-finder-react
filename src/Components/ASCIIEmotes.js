11c6bddb2499f815bf60e01a521092cd897c9256
Diploidal <diploidal@gmail.com>
2020-10-07 22:23:19 +0200

    Add ASCII emojis to new component

--- src/Components/ASCIIEmotes.js
+++ src/Components/ASCIIEmotes.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 export const Emote = () => {
+  const emotesArrray = [
+    // eslint-disable-next-line
+    '¯\_(ツ)_/¯', '(≥o≤)', '(;˚Δ˚)', '(^_^)', "(o_o)", '(>_<)'
+  ];
+  
   return (
-    <div style={{textAlign: 'center'}}>
-      <div>random ASCII emoji</div>
+    <div className="container" style={{textAlign: 'center'}}>
+      <h1 style={{fontSize: '16vw', color: '#dadce0'}}>¯\_(ツ)_/¯</h1>
       <div>Can't find any Github user.</div>
     </div>
   )
